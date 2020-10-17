6d673c7835f8e331b66a9dc1d75f4acfd912e077
Rafa Moterani <hi@rafaismy.name>
2020-10-16 23:12:31 -0400

    fix onSidePaneLoaded not firing properly

--- src/list/events.js
+++ src/list/events.js
@@ -1,16 +1,10 @@
 const SIDE_PANE_SELECTOR = '#pane-side';
-const SIDE_PANE_ROOT_CLASSNAME = 'app';
 
 export const onSidePaneLoaded = (callback) => {
   const paneObserver = new MutationObserver((mutations, observer) => {
     mutations.some((mutation) => {
       const addedNodes = [...mutation.addedNodes];
       return addedNodes.some((node) => {
-        const nodeClass = node.className && node.className.includes && node.className;
-        if (!nodeClass || !nodeClass.includes(SIDE_PANE_ROOT_CLASSNAME)) {
-          return false;
-        }
-
         const sidePane = node.querySelector(SIDE_PANE_SELECTOR);
         if (!sidePane) return false;
 
